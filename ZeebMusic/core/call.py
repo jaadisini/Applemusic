bf1fd36fcf388086350043974e6555a5fbf377ab
Not faund <flaelva6@gmail.com>
2025-06-20 15:33:03 +0700

    Update call.py

--- ZeebMusic/core/call.py
+++ ZeebMusic/core/call.py
@@ -34,7 +34,6 @@ async def _clear_(chat_id):
     db[chat_id] = []
     await remove_active_video_chat(chat_id)
     await remove_active_chat(chat_id)
-class Call(PyTgCalls):
     def __init__(self):
         self.userbot1 = Client(
             name="assistant1",
